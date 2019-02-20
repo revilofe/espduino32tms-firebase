251484fe2e638b92dcfeba49f6fdee5d1b3fdd8d
revilofe <revilofe@gmail.com>
2019-02-20 21:53:20 +0100

    Cambios para evitar usar Delay que facilitan que el hilo siga ejecutandose y realizando otras cosas.

--- espduino32tms-firebase/espduino32tms-firebase.ino
+++ espduino32tms-firebase/espduino32tms-firebase.ino
@@ -10,22 +10,39 @@ RealTimeStore realTimeStore(PATHFB);
 float temperature = 0.0;
 float humedity = 0.0;
 
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0; // will store last time LED was updated
+
+// constants won't change:
+const long interval = 1000; // interval at which to blink (milliseconds)
+
 void setup()
 {
 
   Serial.begin(115200);
   connectWIFI();
-  realTimeStore.connect(FIREBASE_HOST,FIREBASE_AUTH);
-
+  realTimeStore.connect(FIREBASE_HOST, FIREBASE_AUTH);
 }
 
 void loop()
 {
-  temperature = tmsTemperatureRead();
-  humedity = tmsHumedityRead();
-  realTimeStore.sendIntValue(TEMPERATURE, temperature);
-  realTimeStore.sendIntValue(HUMEDITY, humedity);
-  delay(10000);
+  // here is where you'd put code that needs to be running all the time.
+
+  // check to see if it's time to read temperature & Humedity; that is, if the difference
+  // between the current time and last time you read temperature & Humedity is bigger than
+  // the interval at which you want to read temperature & Humedity.
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval)
+  {
+    // save the last time you read Temperature & Humedity
+    previousMillis = currentMillis;
+
+    temperature = tmsTemperatureRead();
+    humedity = tmsHumedityRead();
+    realTimeStore.sendIntValue(TEMPERATURE, temperature);
+    realTimeStore.sendIntValue(HUMEDITY, humedity);
+  }
 }
 
 void connectWIFI()
